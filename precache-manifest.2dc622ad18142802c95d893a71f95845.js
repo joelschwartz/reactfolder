e32d85304adb34f0e19c1689acc37d814ae029fc
joelschwartz <andaconda1000@gmail.com>
2020-07-19 19:34:14 -0400

    Updates

---
+++ precache-manifest.2dc622ad18142802c95d893a71f95845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb1982de9726ffa915d143351499cf8",
+    "url": "/joelschwartz/reactfolder.git/index.html"
+  },
+  {
+    "revision": "01219b2b75052602e581",
+    "url": "/joelschwartz/reactfolder.git/static/css/main.8ed6ba94.chunk.css"
+  },
+  {
+    "revision": "a2d486f41be5deaafcc3",
+    "url": "/joelschwartz/reactfolder.git/static/js/2.59b40566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joelschwartz/reactfolder.git/static/js/2.59b40566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01219b2b75052602e581",
+    "url": "/joelschwartz/reactfolder.git/static/js/main.025d9393.chunk.js"
+  },
+  {
+    "revision": "2cd416d97a9f16249e4a",
+    "url": "/joelschwartz/reactfolder.git/static/js/runtime-main.a3b4f59a.js"
+  }
+]);
