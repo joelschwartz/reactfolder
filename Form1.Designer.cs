8a617f59d3e15263cd49539e693ee6531176b902
joelschwartz <andaconda1000@gmail.com>
2020-08-11 22:54:01 -0400

    base converter

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace baseconverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.hextointeger = new System.Windows.Forms.TextBox();
+			this.hextointlabel = new System.Windows.Forms.Label();
+			this.decimaltohex = new System.Windows.Forms.TextBox();
+			this.decimaltohexlabel = new System.Windows.Forms.Label();
+			this.binarytohex = new System.Windows.Forms.TextBox();
+			this.bintohexlabel = new System.Windows.Forms.Label();
+			this.binarytoint = new System.Windows.Forms.TextBox();
+			this.bintointlabel = new System.Windows.Forms.Label();
+			this.buttonone = new System.Windows.Forms.Button();
+			this.buttontwo = new System.Windows.Forms.Button();
+			this.buttonthree = new System.Windows.Forms.Button();
+			this.buttonfour = new System.Windows.Forms.Button();
+			this.clear = new System.Windows.Forms.Button();
+			this.output = new System.Windows.Forms.Label();
+			
+			this.hextointlabel.Location = new System.Drawing.Point(166, 20);
+			this.hextointlabel.Size = new System.Drawing.Size(350, 30);
+			this.hextointlabel.Name = "hex to integer label";
+			this.hextointlabel.Text = "Please enter a hex string to convert to a integer";
+			this.hextointlabel.TabIndex = 0;
+			
+			this.hextointeger.Location = new System.Drawing.Point(166, 55);
+            this.hextointeger.Name = "hex to integer";
+            this.hextointeger.Size = new System.Drawing.Size(483, 20);
+            this.hextointeger.TabIndex = 1;
+			
+			this.buttonone.Location = new System.Drawing.Point(166, 95);
+            this.buttonone.Name = "submit";
+            this.buttonone.Size = new System.Drawing.Size(75, 23);
+            this.buttonone.TabIndex = 2;
+            this.buttonone.Text = "submit";
+            this.buttonone.UseVisualStyleBackColor = true;
+            this.buttonone.Click += new System.EventHandler(this.buttonone_Click);
+			
+			this.decimaltohexlabel.Location = new System.Drawing.Point(166, 120);
+			this.decimaltohexlabel.Size = new System.Drawing.Size(350, 30);
+			this.decimaltohexlabel.Name = "integer to hex label";
+			this.decimaltohexlabel.Text = "Please enter a integer to convert to a hex string";
+			this.decimaltohexlabel.TabIndex = 3;
+			
+			this.decimaltohex.Location = new System.Drawing.Point(166, 140);
+            this.decimaltohex.Name = "integer to hex";
+            this.decimaltohex.Size = new System.Drawing.Size(483, 20);
+            this.decimaltohex.TabIndex = 4;
+			
+			this.buttontwo.Location = new System.Drawing.Point(166, 170);
+            this.buttontwo.Name = "submit";
+            this.buttontwo.Size = new System.Drawing.Size(75, 23);
+            this.buttontwo.TabIndex = 5;
+            this.buttontwo.Text = "submit";
+            this.buttontwo.UseVisualStyleBackColor = true;
+            this.buttontwo.Click += new System.EventHandler(this.buttontwo_Click);
+			
+			this.bintohexlabel.Location = new System.Drawing.Point(166, 200);
+			this.bintohexlabel.Size = new System.Drawing.Size(350, 30);
+			this.bintohexlabel.Name = "binary to hex label";
+			this.bintohexlabel.Text = "Please enter a binary string to convert to a hex string";
+			this.bintohexlabel.TabIndex = 6;
+			
+			this.binarytohex.Location = new System.Drawing.Point(166, 220);
+            this.binarytohex.Name = "binary to hex";
+            this.binarytohex.Size = new System.Drawing.Size(483, 20);
+            this.binarytohex.TabIndex = 7;
+			
+			this.buttonthree.Location = new System.Drawing.Point(166, 250);
+            this.buttonthree.Name = "submit";
+            this.buttonthree.Size = new System.Drawing.Size(75, 23);
+            this.buttonthree.TabIndex = 8;
+            this.buttonthree.Text = "submit";
+            this.buttonthree.UseVisualStyleBackColor = true;
+            this.buttonthree.Click += new System.EventHandler(this.buttonthree_Click);
+			
+			this.bintointlabel.Location = new System.Drawing.Point(166, 275);
+			this.bintointlabel.Size = new System.Drawing.Size(350, 30);
+			this.bintointlabel.Name = "binary to integer label";
+			this.bintointlabel.Text = "Please enter a binary string to convert to a integer";
+			this.bintointlabel.TabIndex = 9;
+			
+			this.binarytoint.Location = new System.Drawing.Point(166, 295);
+            this.binarytoint.Name = "binary to hex";
+            this.binarytoint.Size = new System.Drawing.Size(483, 20);
+            this.binarytoint.TabIndex = 10;
+			
+			this.buttonfour.Location = new System.Drawing.Point(166, 345);
+            this.buttonfour.Name = "submit";
+            this.buttonfour.Size = new System.Drawing.Size(75, 23);
+            this.buttonfour.TabIndex = 11;
+            this.buttonfour.Text = "submit";
+            this.buttonfour.UseVisualStyleBackColor = true;
+            this.buttonfour.Click += new System.EventHandler(this.buttonfour_Click);
+			
+			this.clear.Location = new System.Drawing.Point(250, 345);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 12;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+			
+			
+			this.output.Location = new System.Drawing.Point(166, 380);
+			this.output.Size = new System.Drawing.Size(350, 30);
+			this.output.Name = "output";
+			this.output.TabIndex = 13;
+			
+			
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Text = "base converter";
+			this.Controls.Add(this.hextointeger);
+			this.Controls.Add(this.hextointlabel);
+			this.Controls.Add(this.decimaltohex);
+			this.Controls.Add(this.decimaltohexlabel);
+			this.Controls.Add(this.binarytohex);
+			this.Controls.Add(this.bintohexlabel);
+			this.Controls.Add(this.binarytoint);
+			this.Controls.Add(this.bintointlabel);
+			this.Controls.Add(this.buttonone);
+			this.Controls.Add(this.buttontwo);
+			this.Controls.Add(this.buttonthree);
+			this.Controls.Add(this.buttonfour);
+			this.Controls.Add(this.clear);
+			this.Controls.Add(this.output);
+			this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+		private System.Windows.Forms.TextBox hextointeger;
+		private System.Windows.Forms.Label hextointlabel;
+		private System.Windows.Forms.TextBox decimaltohex;
+		private System.Windows.Forms.Label decimaltohexlabel;
+		private System.Windows.Forms.TextBox binarytohex;
+		private System.Windows.Forms.Label bintohexlabel;
+		private System.Windows.Forms.TextBox binarytoint;
+		private System.Windows.Forms.Label bintointlabel;
+		private System.Windows.Forms.Button buttonone;
+		private System.Windows.Forms.Button buttontwo;
+		private System.Windows.Forms.Button buttonthree;
+		private System.Windows.Forms.Button buttonfour;
+		private System.Windows.Forms.Button clear;
+		private System.Windows.Forms.Label output;
+		
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace baseconverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+		public static string decimaltobin(int input)
+		{
+			string output = "";
+			string display = "";
+			int counter = 0;
+			int count = 0;
+			int diff = 0;
+			int plus = 3;
+			int total = input;
+			while (Convert.ToInt32(Math.Pow(2, counter)) <= input-Convert.ToInt32(Math.Pow(2, counter)))
+			{
+				counter += 1;
+			}
+			
+				while ( plus < counter || counter-plus != 4 || counter < plus)
+				{
+					if (counter < plus)
+					{
+						diff = plus-counter;
+						while (diff > 0)
+						{
+							output += "0";
+							diff -= 1;
+						}
+						break;
+						
+					}
+					plus += 4;
+				}
+				
+					
+			for (int z = counter; z >= 0; z--)
+			{
+				if (Convert.ToInt32(Math.Pow(2, z)) <= total)
+				{
+					output += "1";
+					total -= Convert.ToInt32(Math.Pow(2, z));
+
+				}
+				else
+				{
+					
+					output += "0";
+				}
+			}
+			for (int x = 0; x < output.Length; x++)
+			{
+				if (count == 4)
+				{
+					display += " ";
+					display += Convert.ToString(output[x]);
+					count = 1;
+				}
+				else
+				{
+					display += Convert.ToString(output[x]);
+					count += 1;
+				}
+			}
+			return display;
+		}
+		public static int binarytodecimal(string bin)
+		{
+			char[] array = new char[bin.Length];
+			int counter = 0;
+			int total = 0;
+			char one = Convert.ToChar("1");
+			for (int a = array.Length-1; a >= 0; a--)
+			{
+				array[counter] = bin[a];
+				counter += 1;
+			}
+			for (int l = array.Length-1; l >= 0; l--)
+			{
+				if (array[l] == one)
+				{
+					total += Convert.ToInt32(Math.Pow(2, l));
+				}
+				
+			}
+			
+			return total;
+		}
+		
+		public static string bintohex(string bin)
+		{
+			int total = binarytodecimal(bin);
+			string output = integertohex(total);
+			return output;
+			
+		}
+		public static string integertohex(int input)
+		{
+			int total = input;
+			string hexstring = "";
+			string binary = decimaltobin(total);
+			char[] hexdigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+			string[] four = binary.Split(" "); 
+			int[] integer = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+			if (input <= 2147483647 && input >= 0)
+			{
+				for (int y = 0; y < four.Length; y++)
+				{
+					for (int x = hexdigits.Length-1; x >= 0; x--)
+					{
+						if (binarytodecimal(Convert.ToString(four[y])) == integer[x])
+						{
+							hexstring += Convert.ToString(hexdigits[x]);
+						}
+					}
+				}
+			}
+			if (Convert.ToString(hexstring[0]) == "0" && hexstring.Length > 1)
+			{
+				hexstring = "" + hexstring.Substring(1);
+			}
+			return hexstring;
+		}
+		public static int hextodecimal(string code)
+		{
+			int total = 0;
+			char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+			int[] number = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+			string input = code.ToLower();
+			char[] array = new char[input.Length];
+			int count = 0;
+			for (int x = input.Length-1; x >= 0; x--)
+			{
+				array[count] = input[x];
+				count += 1;
+			}
+			for (int length = array.Length-1; length >= 0; length--)
+			{
+				for (int letter = 0; letter < hex.Length; letter++)
+				{
+					if (array[length] == hex[letter])
+					{
+						total += Convert.ToInt32(Math.Pow(16, length))*number[letter];
+						letter = 0;
+						break;
+					}
+					
+				}
+			}
+			return total;
+		
+			
+		}
+		private void buttonone_Click(object sender, EventArgs e)
+		{
+			int counter = 0;
+			string hex = hextointeger.Text.ToLower().Replace(" ", "");
+			char[] array = new char[hex.Length];
+			for (int x = 0; x < hex.Length; x++)
+			{
+				array[counter] = Convert.ToChar(hex[x]);
+				counter += 1;
+			}
+			bool hexdigit = true;
+			char[] letter = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+			for (int l = 0; l < array.Length; l++)
+			{
+				for (int c = 0; c < letter.Length; c++)
+				{
+					if (array[l] == letter[c])
+					{
+						break;
+					}
+					else if (c == letter.Length-1)
+					{
+						hexdigit = false;
+						break;
+					}
+					
+					
+				}
+				if (hexdigit == false)
+				break;
+			}
+			if (hex != null)
+			{
+				if (hextodecimal(hex) >= 0 && hexdigit == true)
+				{
+					output.Text = Convert.ToString(hextodecimal(hex));
+				}
+				else
+				{
+					output.Text = "You did not enter a valid hex string";
+				}
+				
+			}
+		}
+		
+		private void buttontwo_Click(object sender, EventArgs e)
+		{
+			string input = decimaltohex.Text;
+			if (Regex.IsMatch(input, @"^\d+$") == true)
+			{
+				int hex = Convert.ToInt32(decimaltohex.Text);
+				output.Text = "";
+				output.Text = integertohex(hex);
+			}
+			else
+			{
+				output.Text = "";
+				output.Text = "not a valid number";
+			}
+			
+		}
+		
+		private void buttonthree_Click(object sender, EventArgs e)
+		{
+			string bin = binarytohex.Text.Replace(" ", "");
+			if (bin != null)
+			{
+				if (Regex.IsMatch(bin, @"[0-1]") == true)
+				{
+					string result = bintohex(bin);
+					output.Text = "";
+					output.Text = result;
+				}
+				else
+				{
+					output.Text = "";
+					output.Text = "not a valid binary string";
+				}
+			}
+			
+		}
+		
+		private void buttonfour_Click(object sender, EventArgs e)
+		{
+			string binary = binarytoint.Text.Replace(" ", "");
+			if (binary != null)
+			{
+				if (Regex.IsMatch(binary, @"[0-1]") == true)
+				{
+					output.Text = "";
+					output.Text = Convert.ToString(binarytodecimal(binary));
+				}
+				else
+				{
+					output.Text = "";
+					output.Text = "not a valid binary string";
+				}
+			}
+		}
+		
+		private void clear_Click(object sender, EventArgs e)
+		{
+			output.Text = "";
+			binarytoint.Text = "";
+			binarytohex.Text = "";
+			decimaltohex.Text = "";
+			hextointeger.Text = "";
+		}
+
+    }
+}
